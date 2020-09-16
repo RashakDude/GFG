5812c86cf9689acf618e4508cc61c549e38af8e8
RashakDude <rajatagarwal686@gmail.com>
2020-09-17 03:49:33 +0530

    Searching and Sorting

---
+++ Searching and Sorting/Square Root.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+long long int floorSqrt(long long int x);
+  
+
+ // } Driver Code Ends
+
+
+// Function to find square root
+// x: element to find square root
+long long int floorSqrt(long long int x) 
+{
+    return sqrt(x);
+    // Your code goes here   
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		cout << floorSqrt(n) << endl;
+	}
+    return 0;   
+}
+  // } Driver Code Ends
